b9fe0da416de186eddb4b78a6731b16acfef84fa
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-09-14 15:39:12 +0200

    P8 Conversion

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿    // See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number.");
+
+string input = Console.ReadLine()!;
+
+double d = Convert.ToDouble(input); 
+
+Console.WriteLine(d);
+
+int i = (int) d;
+
+Console.WriteLine(i);
+
+int number = int.Parse(input);
+
+Console.WriteLine(number);
