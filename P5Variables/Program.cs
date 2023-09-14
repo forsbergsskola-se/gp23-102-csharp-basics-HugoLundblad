da266f7108c27439e2f428049f7054286993e127
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-09-14 15:35:35 +0200

    P5 Variables

---
+++ P5Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 5;
+
+Console.WriteLine(number);
+
+number = 10;
+
+Console.WriteLine(number);
+
+/*int money;
+
+money = 5;
+
+var moola = money * 2;
+
+Console.WriteLine(money);
+Console.WriteLine(moola);*/
