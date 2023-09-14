22f7a999be5efd4d58159cb10aed956d1df9acb4
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-09-14 15:54:40 +0200

    P11 If Else

---
+++ P11.1Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Numerical score:");
+
+int score = int.Parse(Console.ReadLine());
+
+if (score < 60)
+{
+    Console.WriteLine("F");
+}
+else if (score <= 69)
+{
+    Console.WriteLine("D");
+} 
+else if (score <= 79)
+{
+    Console.WriteLine("C");
+} 
+else if (score <= 89)
+{
+    Console.WriteLine("B");
+} 
+else {
+    Console.WriteLine("A");
+}
---
+++ P11.4Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualBasic.CompilerServices;
+
+Console.WriteLine("Input first a number, then an operator, then another number.");
+
+int x = int.Parse(Console.ReadLine());
+char op = char.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+// var OpInput = Convert.ToChar(op);
+
+if (op == '+')
+{
+    Console.WriteLine(x + y);
+}
+else if (op == '-')
+{
+    Console.WriteLine(x - y);
+}
+else if (op == '*')
+{
+    Console.WriteLine(x * y);
+}
+else if (op == '/')
+{
+    Console.WriteLine(x / y);
+}
+else if (op == '%')
+{
+    Console.WriteLine(x % y);
+}
+else 
+{
+    Console.WriteLine("Operator not found");
+}
