8b947c359eb8aa81d0f0a6f0e48d4ad5e8a30370
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-09-14 15:56:01 +0200

    P13 Go To

---
+++ P13.1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("How many dollars do you want?");
+
+int userInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Here's your Dollars: {new string ('|', userInput)}");
---
+++ P13.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Input triangle size.");
+TRIANGLE_VALID:
+int userInput = int.Parse(Console.ReadLine());
+if (userInput < 2)
+{
+    Console.WriteLine("Try a larger number.");
+    goto TRIANGLE_VALID;
+}
+TRIANGLE_GEN:
+if (userInput > 0)
+{
+    Console.WriteLine($"{new string ('#', userInput)}");
+    userInput--;
+    goto TRIANGLE_GEN;
+}
